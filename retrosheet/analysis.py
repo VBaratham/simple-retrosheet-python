c3f5ea3fe8cb062c363261f37ef5aab747962044
Vyassa Baratham <vbaratham@gmail.com>
2021-02-16 21:40:17 -0800

    structs for Event file

--- retrosheet/analysis.py
+++ retrosheet/analysis.py
@@ -20,7 +20,8 @@ class Analysis(object):
         """
         # TODO: allow passing year(s) to fetch the sheet from the internet on the fly
         if filename and filenames:
-            raise ValueError("Use either @filename or @filenames, not both. Alternatively, send all data through STDIN")
+            raise ValueError("Use either @filename or @filenames, not both." + \
+                             " Or send all data through STDIN and use neither.")
         
         if filenames:
             self.filenames = filenames
@@ -30,40 +31,59 @@ class Analysis(object):
             self.filenames = None
 
         self.handlers = OrderedDict()
+        self.stream = io.StringIO()
 
-    def _get_raw_stream(self):
-        """
-        Check whether files or STDIN is being used;
-        return an iterator over all lines of the retrosheet
-        """
-        if self.filenames is not None:
-            for fn in self.filenames:
-                with open(fn) as retrosheet:
-                    for line in retrosheet:
-                        yield line
-        else:
-            for line in SYS.STDIN:
-                yield line
+    def _retrosheet_as_filelike(self):
+        """ Return the entire retrosheet as a single (lazily-loaded) file-like """
+        def _yield_files(self):
+            if self.filenames is not None:
+                for fn in self.filenames:
+                    with open(fn) as infile:
+                        yield infile
+            else:
+                yield sys.stdin
 
+        return itertools.chain(self._yield_files())
+            
     def _get_python_stream(self):
         """ Yield all lines in the retrosheet as Python objects """
-        for line in self._get_raw_stream():
+        reader = csv.reader(self._retrosheet_as_filelike())
+        for line in reader:
             yield pythonify_line(line)
+            
+    # def _get_raw_stream(self):
+    #     """
+    #     Check whether files or STDIN is being used;
+    #     return an iterator over all lines of the retrosheet
+    #     """
+    #     if self.filenames is not None:
+    #         for fn in self.filenames:
+    #             with open(fn) as retrosheet:
+    #                 for line in retrosheet:
+    #                     yield line
+    #     else:
+    #         for line in sys.stdin:
+    #             yield line
 
     def register_handler(self, name, handler):
         """
         Register a handler for this analysis
-        @param name - the name of this handler, for later reference when we go to fetch its' data
+        @param name - the name of this handler, for later reference when we go
+                      to fetch its' data
         @param handler - the handler object
         """
         self.handlers[name] = handler
 
-    def fire_trigger(self, trigger):
+    def register_trigger(self, name, trigger):
+        self.triggers[name] = trigger
+
+    def fire_triggers(self, trigger_names):
         """
-        When a handler returns a trigger, look for a method of this
-        object with that name, and call it
+        When a handler returns a trigger, look for a trigger registered under
+        the given names and call them
         """
-        getattr(self, trigger)()
+        for trigger_name in trigger_names:
+            self.triggers[trigger_name]()
 
     def preprocess(self):
         pass
@@ -76,10 +96,12 @@ class Analysis(object):
         self.preprocess()
         
         for pyline in self._get_python_stream():
+            triggers_to_fire = []
             for handler in self.handlers.values():
-                trigger = handler.handle(pyline)
-                if trigger:
-                    self.fire_trigger(trigger)
+                new_trigger = handler.handle(pyline)
+                if new_trigger:
+                    triggers_to_fire.append(new_trigger)
+            self.fire_triggers(trigger_to_fire)
 
         self.postprocess()
                 
--- retrosheet/event.py
+++ retrosheet/event.py
@@ -4,5 +4,66 @@ Simple structs for Pythonifying the Event file from the MLB retrosheet
 
 __author__ = "Vyassa Baratham"
 
-def pythonify_line(line):
+# TODO: home/away as named enum
+
+class EventLine(object):
+    """
+    Base class for a line from the event file
+    """
     pass
+
+class ID(EventLine):
+    def __init__(self, ID, gameID):
+        self.ID = ID
+        self.gameID = gameID
+
+class Start(EventLine):
+    def __init__(self, playerID, playername, homeaway, battingorder, position):
+        self.playerID = playerID
+        self.playername = playername
+        self.homeaway = homeaway
+        self.battingorder = battingorder
+        self.position = position
+
+class Sub(Start):
+    """ 'sub' and 'start' lines have the exact same format """
+    pass
+
+class Play(EventLine):
+    # TODO: Create a separate parser for the "play" subfield
+    def __init__(self, inning, homeaway, playerID, count, pitches, play):
+        self.inning = inning
+        self.homeaway = homeaway
+        self.playerID = playerID
+        self.count = count
+        self.pitches = pitches
+        self.play = play
+
+class Info(EventLine):
+    def __init__(self, fieldname, data):
+        self.fieldname = fieldname
+        self.data = data
+
+class Com(EventLine):
+    def __init__(self, comment):
+        self.comment = comment
+
+    
+class_for = {
+    'id': ID,
+    'start': Start,
+    'sub': Sub,
+    'play': Play,
+    'info': Info,
+    'com': Com,
+}
+    
+def pythonify_line(line):
+    """
+    Turn one parsed line from the event file into one of the objects defined in this file
+    @param line - array where each element is one field from one line of the event file
+    """
+    cls = class_for(line[0])
+    return cls.from_arr(*line[1:])
+
+
