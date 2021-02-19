ae333bc0c444553ba1f54c05771aa9c82ace0038
Vyassa Baratham <vbaratham@gmail.com>
2021-02-18 18:30:05 -0800

    now processing 5 games

--- retrosheet/handlers/inning.py
+++ retrosheet/handlers/inning.py
@@ -35,8 +35,8 @@ class Inning(Handler):
         return self.inning + (float(self.tot_outs) / 3.0)
 
     def handle_play(self, play):
-        if self.inning != play.inning:
-            raise Exception("Lost track of # outs or inning. We think the inning is: {} with {} out. Current play (not processed yet): {}".format(self.inning, self.out, play))
+        if (self.inning != play.inning) or (self.at_bat != play.homeaway):
+            raise Exception("Lost track of # outs or inning. We think the inning is: {} with {} out, at bat = {}. Current play (not processed yet): {}".format(self.inning, self.out, self.at_bat, play))
 
         # TODO: replace this all w/ a regex
 
@@ -59,19 +59,24 @@ class Inning(Handler):
 
         # CAUGHT STEALING
         elif play.event.startswith('CS'):
-            self.tot_outs += 1
+            # A runner is caught stealing iff there was no error on the pickoff attempt
+            if play.event.find('E') == -1:
+                self.tot_outs += 1
 
         # PICKOFF
-        elif play.event.startswith('PO'):
             # A runner is picked off iff there was no error on the pickoff attempt
+        elif play.event.startswith('PO'):
             if play.event.find('E') == -1:
                 self.tot_outs += 1
                 
         # FC for fielder's choice would be accompanied by X if an out was made
         # We take care of those below
     
-        # Record an out for each runner caught advancing, indicated by X
-        self.tot_outs += play.event.count('X')
+        # Record an out for each runner caught advancing, indicated by
+        # X in the "advance" part of the event string, which starts
+        # after "."
+        if '.' in play.event:
+            self.tot_outs += play.event.split('.')[-1].count('X')
 
-        print("Inning {}, {} out".format(self.inning, self.out))
+        print("Inning {}, {} out, at bat = {}".format(self.inning, self.out, self.at_bat))
 
